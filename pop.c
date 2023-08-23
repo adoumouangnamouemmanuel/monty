35dbc96a5dd62768070f83adf7bf252b68ee7269
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2023-08-23 11:35:36 -0700

    stack, queue

--- pop.c
+++ pop.c
@@ -1,23 +1,30 @@
 #include "monty.h"
-/**
- * f_pop - prints the top
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
-void f_pop(stack_t **head, unsigned int counter)
-{
-	stack_t *h;
 
-	if (*head == NULL)
+/**
+ * get_pop - function that pops the top element of the stack
+ * @stack: pointer to the top of the stack
+ * @line_number: where the line number appears
+ * Description: 2. pop
+ * Return: see below
+ * 1. upon success, nothing
+ * 2. upon fail, EXIT_FAILURE
+ */
+void get_pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *first;
+
+	if ((*stack == NULL) || (stack == NULL))
 	{
-		fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		fclose(file);
+		get_free(*stack);
 		exit(EXIT_FAILURE);
 	}
-	h = *head;
-	*head = h->next;
-	free(h);
+	first = *stack;
+	*stack = (*stack)->next;
+	if (first->next)
+	{
+		first->next->prev = NULL;
+	}
+	free(first);
 }
